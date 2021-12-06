c979d0f1f6464b8a6d2eb1bce4df4c2ab9a0d5c8
terrycojones <terry@jon.es>
2021-12-06 08:41:42 +0100

    Use lru_cache. Function rename. Remove slow original solution to part 1.

--- day-06/puzzle.py
+++ day-06/puzzle.py
@@ -1,47 +1,24 @@
 import sys
+from functools import lru_cache
 
-import numpy as np
 
+@lru_cache(maxsize=None)
+def countFish(args):
+    state, days = args
 
-def calc(state, days, cache):
     if days == 0:
         return 1
 
-    result = cache[state, days]
+    if state == 0:
+        newState = 6
+        offspring = countFish((8, days - 1))
+    else:
+        newState = state - 1
+        offspring = 0
 
-    if result == 0:
-        if state == 0:
-            newState = 6
-            offspring = calc(8, days - 1, cache)
-        else:
-            newState = state - 1
-            offspring = 0
-
-        result = calc(newState, days - 1, cache) + offspring
-        cache[state, days] = result
-
-    return result
+    return countFish((newState, days - 1)) + offspring
 
 
 def main(days):
     states = list(map(int, sys.stdin.read().rstrip().split(',')))
-    cache = np.zeros((9, days + 1), dtype=int)
-    return sum(calc(state, days, cache) for state in states)
-
-
-def mainSlow(days):
-    # Original solution to part 1.
-    states = list(map(int, sys.stdin.read().rstrip().split(',')))
-
-    for day in range(days):
-        eights = 0
-        for i, fish in enumerate(states):
-            if fish == 0:
-                eights += 1
-                states[i] = 6
-            else:
-                states[i] -= 1
-
-        states.extend([8] * eights)
-
-    return len(states)
+    return sum(countFish((state, days)) for state in states)
