2f4652d1dee7dfa503e6150e7ddb539d590089d6
terrycojones <terry@jon.es>
2021-12-07 09:07:41 +0100

    Changed variable name.

--- day-07/puzzle.py
+++ day-07/puzzle.py
@@ -1,8 +1,8 @@
 import sys
 
 
-def main(moveCost):
+def main(cost):
     positions = list(map(int, sys.stdin.read().rstrip().split(',')))
 
-    return min(sum(moveCost(abs(position - p)) for position in positions)
+    return min(sum(cost(abs(position - p)) for position in positions)
                for p in range(min(positions), max(positions) + 1))
