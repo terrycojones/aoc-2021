4945fd32bd2b53cc7d9fcc7780fdb25edfeac9dc
terrycojones <terry@jon.es>
2021-12-07 10:05:42 +0100

    Removed unneeded input rstrip() call

--- day-07/puzzle.py
+++ day-07/puzzle.py
@@ -2,7 +2,7 @@ import sys
 
 
 def main(cost):
-    positions = list(map(int, sys.stdin.read().rstrip().split(',')))
+    positions = list(map(int, sys.stdin.read().split(',')))
 
     return min(sum(cost(abs(position - answer)) for position in positions)
                for answer in range(min(positions), max(positions) + 1))
