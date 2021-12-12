67d4bdbf8ca7d6bfde463ffb514b74c8fab7be76
terrycojones <terry@jon.es>
2021-12-12 07:44:12 +0100

    Added day 12

---
+++ day-12/puzzle.py
@@ -0,0 +1,35 @@
+import sys
+from collections import defaultdict
+
+
+def main(pathClass):
+    edges = defaultdict(set)
+    vertices = set()
+    for line in sys.stdin:
+        start, end = line.rstrip().split('-')
+        edges[start].add(end)
+        edges[end].add(start)
+        vertices.update((start, end))
+
+    assert 'start' in vertices
+    p = pathClass()
+    p.extend('start')
+
+    incomplete = {p}
+    complete = set()
+
+    while incomplete:
+        path = incomplete.pop()
+
+        for vertex in edges[path.last()]:
+            if path.okAsNext(vertex):
+                new = path.copy()
+                new.extend(vertex)
+                if new.complete():
+                    if new not in complete:
+                        complete.add(new)
+                else:
+                    if new not in incomplete:
+                        incomplete.add(new)
+
+    return len(complete)
